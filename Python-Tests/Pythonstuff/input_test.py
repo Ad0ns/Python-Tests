35c89685a3c862024cd6d9104d340b4531660091
Ad0ns <adoniskalaitzis@gmail.com>
2025-06-12 01:48:27 +0300

    FIXED STUFF I THINK

--- Python-Tests/Pythonstuff/input_test.py
+++
@@ -1,4 +0,0 @@
-x = input("best fortner burger ")
-y = input("how many burgr man want? ")
-
-print ("u get" ,y , x , "burger")
--- Python-Tests/Pythonstuff/max_num.py
+++
@@ -1,8 +0,0 @@
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-number3 = int(input("Enter the third number: "))
-
-
-biggest_num = max(number1,number2,number3)
-
-print (biggest_num)
