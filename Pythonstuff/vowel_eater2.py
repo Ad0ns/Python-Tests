f0aae6ac6d3203c9c7cfe00664063a5b909ab77f
Ad0ns <adoniskalaitzis@gmail.com>
2025-06-12 16:05:25 +0300

    vowel eater2

---
+++ Pythonstuff/vowel_eater2.py
@@ -0,0 +1,16 @@
+word = input("Enter a word: ")
+word = word.upper()
+
+for i in word:
+    if i == "A":
+        continue
+    elif i == "E":
+        continue
+    elif i == "I":
+        continue
+    elif i == "O":
+        continue
+    elif i == "U":
+        continue
+    else:
+        print(i)
