c4144b2022e1c134ca1093d9c02fc26616f55243
Ad0ns <adoniskalaitzis@gmail.com>
2025-06-12 15:28:00 +0300

    True loop with break

---
+++ Pythonstuff/break_exercise.py
@@ -0,0 +1,8 @@
+# while True: creates an infinite loop
+# if word is chupacavra it breaks out of the loop with break
+
+while True:
+    word = input("Give thing: ")
+    if word == "chupacabra":
+        print("You've successfully left the loop.")
+        break
